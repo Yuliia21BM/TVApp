37285ac9ab994dc615d5c2091b7541107370ff9c
YuliiaM-UT <108929319+Yuliia21BM@users.noreply.github.com>
2024-12-13 15:28:09 +0200

    feat: update constants and colors

--- src/utils/Colors.ts
+++ src/utils/Colors.ts
@@ -1,13 +1,8 @@
 export const Colors = {
   black: '#000000',
   white: '#ffffff',
-  white30: 'rgba(255,255,255,0.3)',
-  whiteSmoke: '#f0f0f0',
-  lightGray: 'rgba(225,225,225,0.4)',
-  lightGrey: '#cbcbcb',
   dimGray: 'rgba(30,30,30,0.85)',
-  grayBookmarkBar: 'rgba(216,216,216,0.2)',
-  transparent: 'rgba(255, 255, 255, 0)',
-  blue: '#007AFF',
-  buttonBackground: '#191a1c',
+  primaryBackground: '#150A35',
+  red: '#aa0000',
+  pink: '#FE3EA1',
 };
